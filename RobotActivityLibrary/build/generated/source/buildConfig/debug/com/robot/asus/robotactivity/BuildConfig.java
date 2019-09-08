af0b4dca50549e46ed926f20e09521c36d9dc030
pitchayakit <pitchayakit.p@gmail.com>
2019-09-08 15:30:46 +0800

    Create module

--- RobotActivityLibrary/build/generated/source/buildConfig/debug/com/robot/asus/robotactivity/BuildConfig.java
+++ RobotActivityLibrary/build/generated/source/buildConfig/debug/com/robot/asus/robotactivity/BuildConfig.java
@@ -5,6 +5,11 @@ package com.robot.asus.robotactivity;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.robot.asus.robotactivity";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "com.robot.asus.robotactivity";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
